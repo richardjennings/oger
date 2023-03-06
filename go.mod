a5dc1496ec04d717828b1071513a48337727747b
Richard Jennings <richardjennings@gmail.com>
2023-03-06 20:57:43 +0000

    init cmd

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/open-policy-agent/opa v0.49.2
+	github.com/spf13/cobra v1.6.1
 	github.com/stretchr/testify v1.8.1
 )
 
@@ -13,9 +14,11 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tchap/go-patricia/v2 v2.3.1 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
--- main.go
+++ main.go
@@ -1,44 +1,9 @@
 package main
 
 import (
-	"context"
-	"github.com/richardjennings/oger/eval"
-	"io"
-	"io/fs"
-	"log"
-	"os"
-	"path/filepath"
+	"github.com/richardjennings/oger/cmd"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatalln("usage: oger <path/to/rule> </path/to/policies>")
-	}
-
-	ctx := context.Background()
-
-	pf, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	pc, err := io.ReadAll(pf)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	if err := filepath.Walk(os.Args[2], func(path string, info fs.FileInfo, err error) error {
-		if filepath.Ext(path) == ".rego" {
-			log.Printf("processing %s\n", path)
-			f, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			return eval.Eval(ctx, pc, path, f)
-		}
-		return nil
-	}); err != nil {
-		log.Fatalln(err)
-	}
-
+	cmd.Execute()
 }
