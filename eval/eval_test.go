12e946030cf86b5687d564683a3c4adbf019630c
Richard Jennings <richardjennings@gmail.com>
2023-03-06 20:19:38 +0000

    init ext_calls

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -3,24 +3,98 @@ package eval
 import (
 	"bytes"
 	"context"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestEval(t *testing.T) {
-	rule := []byte(`
-package policy
+	type tc struct {
+		rule       string
+		policy     string
+		policyPath string
+		err        error
+	}
+	for _, test := range []tc{
 
-default allow = false
+		{
+			// first statement must be of type *ast.Package
+			rule: `
+		package policy
+		default allow = false
+		allow {
+		    input.statements[0]["_type"] == "*ast.Package"
+		}`,
+			policy: `package kubernetes.admission`,
+			err:    nil,
+		},
 
-# package must be specified as the first statement
-allow {
-    input.statements[0]["_type"] == "*ast.Package"
-}
+		// first statement must be of type *ast.
+		{
+			rule: `
+		package policy
+		default allow = false
+		allow {
+		    input.statements[0]["_type"] == "*ast.package"
+		}`,
+			policy:     `package kubernetes.admission`,
+			policyPath: "some/package.rego",
+			err:        errors.New("some/package.rego failed"),
+		},
 
-`)
-	policy := bytes.NewBufferString(`
-package kubernetes.admission
-`)
-	assert.Nil(t, Eval(context.Background(), rule, "kubernetes/admission.rego", policy))
+		// Allow use of http.send call only by comparing ext_calls to allowed list
+		{
+			rule: `
+		package policy
+		default allow = false
+		valid_ext_calls := { "http.send" }
+		invalid_set := {x | x := input.ext_calls[_]} 
+		invalid := invalid_set - valid_ext_calls
+		allow {
+			count(invalid) == 0
+		}`,
+			policy: `
+		package play
+		default hello = false
+		hello {
+			response := http.send({
+				"method" : "GET",
+				"url": "http://localhost:8181/v1/data/example"
+			})
+		}`,
+			policyPath: "ext_calls.rego",
+			err:        nil,
+		},
+
+		// Deny use of http.send call by comparing ext_calls to allowed list
+		{
+			rule: `
+		package policy
+		default allow = false
+		valid_ext_calls := { "abc.def" }
+		invalid_set := {x | x := input.ext_calls[_]} 
+		invalid := invalid_set - valid_ext_calls
+		allow {
+			count(invalid) == 0
+		}`,
+			policy: `
+		package play
+		default hello = false
+		hello {
+			response := http.send({
+				"method" : "GET",
+				"url": "http://localhost:8181/v1/data/example"
+			})
+		}`,
+			policyPath: "ext_calls.rego",
+			err:        errors.New("ext_calls.rego failed"),
+		},
+	} {
+		assert.Equal(
+			t,
+			test.err,
+			Eval(context.Background(), []byte(test.rule), test.policyPath, bytes.NewBufferString(test.policy)),
+		)
+	}
+
 }
--- inputs/inputs.go
+++ inputs/inputs.go
@@ -11,6 +11,8 @@ func Inputs(stmts []ast.Statement, path string) (map[string]interface{}, error) 
 	var err error
 	r["statements"], err = statements(stmts)
 	r["path"] = path
+	r["ext_calls"] = extCalls(stmts)
+
 	return r, err
 }
 
@@ -18,7 +20,7 @@ func Inputs(stmts []ast.Statement, path string) (map[string]interface{}, error) 
 func statements(ss []ast.Statement) ([]map[string]interface{}, error) {
 	var r []map[string]interface{}
 	var err error
-	stmt := make(map[string]interface{})
+	var stmt map[string]interface{}
 	for _, v := range ss {
 		stmt, err = statement(v)
 		stmt["_type"] = reflect.TypeOf(v).String()
@@ -32,6 +34,31 @@ func statement(s ast.Statement) (map[string]interface{}, error) {
 	var err error
 	var b []byte
 	b, err = json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+	if err != nil {
+		return nil, err
+	}
 	err = json.Unmarshal(b, &r)
 	return r, err
+}
+
+func extCalls(ss []ast.Statement) []string {
+	var calls []string
+	visitor := ast.NewGenericVisitor(func(x interface{}) bool {
+		switch x := x.(type) {
+		case ast.Call:
+			calls = append(calls, x[0].Value.String())
+		}
+		return false
+	})
+	for _, v := range ss {
+		visitor.Walk(v)
+	}
+
+	// find all function declarations in the ast.
+	// extCalls are calls to functions not defined in this AST
+
+	return calls
 }
